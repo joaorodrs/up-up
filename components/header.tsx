f25a2d41e465db85ede0753a8787d56c78223eb5
Joao Paulo Alencar Rodrigues <joao.paulo@inovan.do>
2024-04-28 13:38:57 -0300

    feat(header): finishes desktop version of header

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { MenuIcon, Instagram, Facebook, Twitter } from 'lucide-react'
+import { MenuIcon, Instagram, Facebook, Twitter, ArrowRightIcon } from 'lucide-react'
 
 import { Button } from './ui/button'
 import {
@@ -10,15 +10,27 @@ import {
 
 function Header() {
   return (
-    <header className="w-full p-6 flex justify-between items-center">
-      <Image
-        src="/up-logo.png"
-        alt="Up&Up"
-        width={50}
-        height={50}
-      />
+    <header className="w-full p-6 flex justify-between items-center px-[5%]">
+      <div className="flex items-center space-x-12">
+        <Image
+          src="/up-logo.png"
+          alt="Up&Up"
+          width={50}
+          height={50}
+        />
+        <nav className="hidden md:flex space-x-8">
+          <Button variant="link" className="px-1 text-md text-white">Sobre</Button>
+          <Button variant="link" className="px-1 text-md text-white">Trabalhos</Button>
+          <Button variant="link" className="px-1 text-md text-white">Serviços</Button>
+          <Button variant="link" className="px-1 text-md text-white">Contato</Button>
+        </nav>
+      </div>
+      <Button variant="outline" className="hidden rounded-full border-primary text-primary md:flex">
+        Entre Em Contato
+        <ArrowRightIcon className="ml-2" />
+      </Button>
       <Drawer>
-        <DrawerTrigger className="p-2">
+        <DrawerTrigger className="p-2 md:hidden">
           <MenuIcon />
         </DrawerTrigger>
         <DrawerContent>
@@ -33,11 +45,10 @@ function Header() {
               <h1 className="text-xl font-bold">Up&Up Marketing</h1>
             </div>
             <div className="grid grid-cols-2 mt-8 w-[50%]">
-              <Button variant="link" className="px-1 text-md justify-start">About</Button>
-              <Button variant="link" className="px-1 text-md justify-start">Work</Button>
-              <Button variant="link" className="px-1 text-md justify-start">Service</Button>
-              <Button variant="link" className="px-1 text-md justify-start">Contact</Button>
-              <Button variant="link" className="px-1 text-md justify-start">Pricing</Button>
+              <Button variant="link" className="px-1 text-md justify-start">Sobre</Button>
+              <Button variant="link" className="px-1 text-md justify-start">Trabalhos</Button>
+              <Button variant="link" className="px-1 text-md justify-start">Serviços</Button>
+              <Button variant="link" className="px-1 text-md justify-start">Contato</Button>
             </div>
             <div className="mt-8 space-x-4">
               <Button size="icon" variant="ghost">
